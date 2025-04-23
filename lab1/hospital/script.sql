86a1e2d79831d39361f04bccbc4f25aa87374df4
Daniel Maia <daniel.maia.t0120684@edu.atec.pt>
2025-04-23 22:29:35 +0100

    Update script.sql

--- lab1/hospital/script.sql
+++ lab1/hospital/script.sql
@@ -21,8 +21,8 @@ CREATE TABLE utente (
 -- Criar tabela agendamento
 CREATE TABLE agendamento (
 	id_agendamento INT AUTO_INCREMENT PRIMARY KEY,
-    id_utente INT NOT NULL, -- FK
-    id_funcionario INT NOT NULL, -- FK
+    id_utente INT, -- FK
+    id_funcionario INT, -- FK
     tipo TEXT NOT NULL,
     data_hora DATE NOT NULL,
     creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -34,7 +34,7 @@ CREATE TABLE agendamento (
 -- Criar tabela ficheiro_clinico
 CREATE TABLE ficheiro_clinico (
 	id_ficheiro_clinico INT AUTO_INCREMENT PRIMARY KEY,
-    id_utente INT NOT NULL, -- FK
+    id_utente INT, -- FK
     id_consulta INT, -- FK
     doencas TEXT,
     alergias TEXT,
@@ -48,8 +48,8 @@ CREATE TABLE ficheiro_clinico (
 -- Criar tabela consulta
 CREATE TABLE consulta (
 	id_consulta INT AUTO_INCREMENT PRIMARY KEY,
-    id_agendamento INT NOT NULL, -- FK
-    id_prescricao INT NOT NULL, -- FK
+    id_agendamento INT, -- FK
+    id_prescricao INT, -- FK
     id_exame INT, -- FK
 	creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
@@ -66,7 +66,7 @@ CREATE TABLE funcionario (
     nif VARCHAR(20) NOT NULL, 
     email VARCHAR(50) NOT NULL,
     num_telefone VARCHAR(9) NOT NULL,
-	id_departamento INT NOT NULL, -- FK
+	id_departamento INT, -- FK
 	creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 	-- FOREIGN KEY (id_departamento) REFERENCES departamento(id_departamento)
@@ -76,7 +76,7 @@ CREATE TABLE funcionario (
 CREATE TABLE departamento (
     id_departamento INT AUTO_INCREMENT PRIMARY KEY,
     nome TEXT NOT NULL,
-    id_responsavel INT NOT NULL, -- FK
+    id_responsavel INT, -- FK
 	creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     -- FOREIGN KEY (id_responsavel) REFERENCES funcionario(id_funcionario)
@@ -85,8 +85,8 @@ CREATE TABLE departamento (
 -- Criar tabela internamento
 CREATE TABLE internamento (
     id_internamento INT AUTO_INCREMENT PRIMARY KEY,
-    id_utente INT NOT NULL, -- FK
-    id_quarto INT NOT NULL, -- FK
+    id_utente INT, -- FK
+    id_quarto INT, -- FK
     data_inicio DATE NOT NULL, 
     data_fim DATE NOT NULL,
 	creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -108,7 +108,7 @@ CREATE TABLE quarto (
 -- Criar tabela exame
 CREATE TABLE exame (
     id_exame INT AUTO_INCREMENT PRIMARY KEY,
-    id_agendamento INT NOT NULL, -- FK
+    id_agendamento INT, -- FK
     tipo_exame TEXT NOT NULL,
     resultado TEXT NOT NULL,
 	creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -119,7 +119,7 @@ CREATE TABLE exame (
 -- Criar tabela prescricao
 CREATE TABLE prescricao (
     id_prescricao INT AUTO_INCREMENT PRIMARY KEY,
-	id_medicamento INT NOT NULL, -- FK
+	id_medicamento INT, -- FK
 	dosagem TEXT NOT NULL,
     data DATE NOT NULL,
 	creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
